894735e36d64d542c90dc0d3f105f429053732ac
Troy Center <troycenter1@gmail.com>
2017-06-07 16:16:53 -0700

    updated readme.md. Created store.py.

---
+++ store.py
@@ -0,0 +1,12 @@
+'''docstring for the file overall
+by: Troy Center, troycenter1@gmail.com, Coding Dojo Python fundamentals, June 2017
+'''
+
+#pylint: disable=C0103
+
+def function():
+
+    '''Docline for function
+    '''
+
+     pass
